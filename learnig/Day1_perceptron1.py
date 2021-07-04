d8b85ec73480f38bb31f512502c3663dcda301f9
­이석우 / 학생 / 화학생물공학부 <syleelsw@snu.ac.kr>
2021-07-04 14:40:06 +0000

    신경망 공부중 ㅇㅅㅇ

--- learnig/Day1_perceptron1.py
+++ learnig/Day1_perceptron1.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+%matplotlib inline 
 def AND(x1, x2):
     x = np.array([x1,x2])
     w = np.array([0.5,0.5])
@@ -42,8 +43,8 @@ def XOR(x1,x2):
     return AND(y1,y2)
 
 
-test(XOR)
-
+#test(XOR)
+#
     
     
     
@@ -62,3 +63,40 @@ and 1   or 1   0
     0      1   1 
     0      0   1
 '''
+
+
+'''
+-----------------------------0704 공부 내용 시작--------------------------------------
+
+활성화 함수 h(X) -> 기존 퍼셉트론을 한번더 처리를 해주는듯? , 퍼셉트론에서 신경망으로 가는 이정표!
+시그모이드아 계단 함수의 차이: 연속적인 값 반환! 그러나 크게 보면 둘이비슷함(수렴)
+공통점: 비선형 함수 
+
+과거에는 시그모이드 많이 사용했으나
+최근에는 ReLu많이 사용. -> 0이하면0 출력, 그이상이면 그대로 출력:
+h(x) = x (x>0) , 0(x<=0)
+
+기계학습은 분류와 회귀로 나뉨. 분류는 어디에 속하느냐, 회귀는 수치를 예측하는 문제 ex): 사진속 인물의 몸무게 예측
+
+분류는 주로 소프트맥스 함수를 씀. -> 항상 모든 출력의 합이 1이됨: 확률로 생ㅇ각 가능!
+
+yK - exp(ak)/sigma_i=1 to n exp(ai) n은 출력층의 뉴런수, k는 n번째 출력임을 의미 
+
+
+출력층의 뉴런수: 분류하고 싶은 클래스 수로 정함!!
+'''
+
+
+def stepfunction(x):
+    y = x>0
+    return y.astype(np.int)
+def sigmoid(x):
+    return 1/ (1+ np.exp(-x))
+def relu(x):
+    return np.maximum(0,x)
+def softma(a):
+    c = np.max(a) # 오버플로 대책
+    exp_a = np.exp(a-c)
+    sum_exp = np.sum(exp_a)
+    y = exp_a/ sum_exp
+    return y
